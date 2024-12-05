1a9bee89cedf8158e82fef09a03099b15544f754
Rémi Caulier <Miunn>
2024-12-05 15:59:07 +0100

    feat: login flow

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,29 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function checkIfTokenValid(token: string) {
+  const r = await fetch(import.meta.env.VITE_STATS_REMOTE_URL + "/check-token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "access_token": token,
+      "token_type": "Bearer"
+    })
+  });
+
+  return r.ok;
+}
+
+export async function redirectIfTokenValid(token: string, requestedRoute: string, navigate: (to: string, options?: { replace: boolean }) => void) {
+  const isTokenValid = await checkIfTokenValid(token);
+  if (isTokenValid) {
+    console.log("Redirect to requested route", requestedRoute);
+
+    navigate(requestedRoute);
+    return;
+  }
+  navigate("/login");
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,10 @@
+import { Toaster } from "@/components/ui/toaster";
+
 export default function Home() {
     return (
         <div className="w-full">
             <h1>Home</h1>
+            <Toaster />
         </div>
     );
 }
