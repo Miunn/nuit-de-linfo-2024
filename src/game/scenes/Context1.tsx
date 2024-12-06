d9b7ceb4d188648aac7599a36aebbdfb64aa60b6
robinlafontaine <robin@lafontaines.eu>
2024-12-06 07:02:10 +0100

    fix: add click prompt

--- src/game/scenes/Context1.tsx
+++ src/game/scenes/Context1.tsx
@@ -9,7 +9,7 @@ export class Context1 extends Scene {
     create() {
 
         this.add.text(500, 300, 
-            "Les poumons humains et le phytoplancton des océans sont essentiels à la vie : les premiers échangent les gaz vitaux pour notre corps, tandis que le second produit la moitié de l'oxygène que nous respirons et absorbe le CO₂. Cet équilibre est menacé par la pollution, le réchauffement climatique et d'autres pressions. Protéger nos poumons et nos 'poumons bleus' implique de réduire les polluants, préserver les écosystèmes et adopter des pratiques durables pour garantir la santé de notre planète et de ses habitants.",
+            "Les poumons humains et le phytoplancton des océans sont essentiels à la vie : les premiers échangent les gaz vitaux pour notre corps, tandis que le second produit la moitié de l'oxygène que nous respirons et absorbe le CO₂. Cet équilibre est menacé par la pollution, le réchauffement climatique et d'autres pressions. Protéger nos poumons et nos 'poumons bleus' implique de réduire les polluants, préserver les écosystèmes et adopter des pratiques durables pour garantir la santé de notre planète et de ses habitants.\n\nCLIQUEZ ICI !",
             {
             fontSize: '24px',
             color: '#ffffff',
