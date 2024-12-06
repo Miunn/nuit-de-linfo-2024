f8e41dccc62f7bb0b2bbfb44f096edb509062c6c
Rémi Caulier <Miunn>
2024-12-06 03:09:05 +0100

    Logo link

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -23,7 +23,10 @@ export function Layout() {
             background: "linear-gradient(115deg, #62cff4, #2c67f2)"
         }}>
             <header className={`fixed w-full ${isIndexPage ? "border-b" : ""} z-10 flex justify-stretch items-center`}>
-                <img src="/logo.png" alt="logo" className="h-10 pl-10" />
+                <a href="/" className="w-fit pl-10">
+                    <img src="/logo.png" alt="logo" className="h-10" />
+                </a>
+
                 <ul className=" w-full flex justify-end py-5 pr-10 bg-transparent">
                     <li>
                         <Button variant="link" onClick={onClickPodcasts}>Podcasts</Button></li>
