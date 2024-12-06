246ef2a1e70f1ca25d18af21653032706acb05aa
Rémi Caulier <Miunn>
2024-12-06 02:56:34 +0100

    Cleanup

--- src/routes/Podcasts.tsx
+++ src/routes/Podcasts.tsx
@@ -1,15 +1,7 @@
-import PodcastCard, { PodcastCardProps } from "@/components/PodcastCard";
-import PodcastViewer, { PodcastViewerProps } from "@/components/PodcastViewer";
-import { AnimatePresence, motion } from "framer-motion";
-import { Fragment, useEffect, useId, useRef, useState } from "react";
-import { useOnClickOutside } from 'usehooks-ts';
+import PodcastCard from "@/components/PodcastCard";
+import { Fragment } from "react";
 
 export function Podcasts() {
-    const [active, setActive] = useState<PodcastCardProps | boolean | null>(
-        null
-    );
-    const ref = useRef<HTMLDivElement>(null);
-    const id = useId();
 
     const getPodcasts = () => {
         const podcasts = [
@@ -27,25 +19,6 @@ export function Podcasts() {
         return podcasts;
     }
 
-    useEffect(() => {
-        function onKeyDown(event: KeyboardEvent) {
-          if (event.key === "Escape") {
-            setActive(false);
-          }
-        }
-     
-        if (active && typeof active === "object") {
-          document.body.style.overflow = "hidden";
-        } else {
-          document.body.style.overflow = "auto";
-        }
-     
-        window.addEventListener("keydown", onKeyDown);
-        return () => window.removeEventListener("keydown", onKeyDown);
-      }, [active]);
-     
-      useOnClickOutside(ref, () => setActive(null));
-
     return (
         <div>
             <div className="grid p-10" style={{
@@ -56,7 +29,7 @@ export function Podcasts() {
                     getPodcasts().map((podcast, index) => {
                         return (
                             <Fragment key={`${index}-${podcast.title}`}>
-                                <PodcastCard title={podcast.title} description={podcast.description} imageUrl={podcast.imageUrl} />
+                                <PodcastCard title={podcast.title} description={podcast.description} imageUrl={podcast.imageUrl} videoUrl={"/video.mp4"} />
                             </Fragment>
                         )
                     })
